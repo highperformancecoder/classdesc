26d657f24b0adec49e380da31bc7f8c95b568ca5
Russell Standish <hpcoder@hpcoders.com.au>
2016-09-02 11:48:42 +1000

    Making doc consistent in git

--- doc/classdesc/labels.pl
+++ doc/classdesc/labels.pl
@@ -2,79 +2,11 @@
 # Associate labels original text with physical files.
 
 
-$key = q/cite_92:_gcc/;
-$external_labels{$key} = "$URL/" . q|node48.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/fig10/;
 $external_labels{$key} = "$URL/" . q|node42.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig7/;
-$external_labels{$key} = "$URL/" . q|node40.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/XDR/;
-$external_labels{$key} = "$URL/" . q|node11.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig5/;
-$external_labels{$key} = "$URL/" . q|node39.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig12/;
-$external_labels{$key} = "$URL/" . q|node43.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_daniels00:_swarm_com/;
-$external_labels{$key} = "$URL/" . q|node48.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/is_sequence/;
-$external_labels{$key} = "$URL/" . q|node10.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/is_associative_container/;
-$external_labels{$key} = "$URL/" . q|node10.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_johnson:_swarm_user_guide/;
-$external_labels{$key} = "$URL/" . q|node48.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/CLASSDESC_ACCESS/;
-$external_labels{$key} = "$URL/" . q|node8.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/slave-method/;
-$external_labels{$key} = "$URL/" . q|node31.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/symbolic_enums/;
-$external_labels{$key} = "$URL/" . q|node23.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig8/;
-$external_labels{$key} = "$URL/" . q|node40.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig9/;
-$external_labels{$key} = "$URL/" . q|node41.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig14/;
-$external_labels{$key} = "$URL/" . q|node45.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig4/;
-$external_labels{$key} = "$URL/" . q|node39.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/polymorph-example/;
-$external_labels{$key} = "$URL/" . q|node18.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_kernighan88:_c/;
+$key = q/cite_98:_objec_c_languag_rhaps_devel_docum/;
 $external_labels{$key} = "$URL/" . q|node48.html|; 
 $noresave{$key} = "$nosave";
 
@@ -82,71 +14,139 @@ $key = q/fig15/;
 $external_labels{$key} = "$URL/" . q|node45.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig2/;
-$external_labels{$key} = "$URL/" . q|node37.html|; 
+$key = q/is_associative_container/;
+$external_labels{$key} = "$URL/" . q|node10.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/library-method/;
-$external_labels{$key} = "$URL/" . q|node4.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec2.6/;
-$external_labels{$key} = "$URL/" . q|node42.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig3/;
-$external_labels{$key} = "$URL/" . q|node38.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_02:_insid_mac_os_x/;
-$external_labels{$key} = "$URL/" . q|node48.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/classdesc/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig6/;
-$external_labels{$key} = "$URL/" . q|node40.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/classdesc_epilogue/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig1/;
-$external_labels{$key} = "$URL/" . q|node37.html|; 
+$key = q/XDR/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_larkin93:_objec_orien_progr_objec_c/;
 $external_labels{$key} = "$URL/" . q|node48.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_Madina-Standish01/;
+$key = q/cite_92:_gcc/;
 $external_labels{$key} = "$URL/" . q|node48.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_98:_objec_c_languag_rhaps_devel_docum/;
-$external_labels{$key} = "$URL/" . q|node48.html|; 
+$key = q/slave-method/;
+$external_labels{$key} = "$URL/" . q|node31.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig11/;
-$external_labels{$key} = "$URL/" . q|node43.html|; 
+$key = q/fig1/;
+$external_labels{$key} = "$URL/" . q|node37.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/is_sequence/;
+$external_labels{$key} = "$URL/" . q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig7/;
+$external_labels{$key} = "$URL/" . q|node40.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig13/;
 $external_labels{$key} = "$URL/" . q|node44.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/pointers/;
-$external_labels{$key} = "$URL/" . q|node12.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/ref/;
 $external_labels{$key} = "$URL/" . q|node14.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/cite_Madina-Standish01/;
+$external_labels{$key} = "$URL/" . q|node48.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_daniels00:_swarm_com/;
+$external_labels{$key} = "$URL/" . q|node48.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig2/;
+$external_labels{$key} = "$URL/" . q|node37.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/polymorphism/;
+$external_labels{$key} = "$URL/" . q|node18.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/classdesc/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/library-method/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig3/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_johnson:_swarm_user_guide/;
+$external_labels{$key} = "$URL/" . q|node48.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/classdesc_epilogue/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig12/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig14/;
+$external_labels{$key} = "$URL/" . q|node45.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/pointers/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig9/;
+$external_labels{$key} = "$URL/" . q|node41.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig5/;
+$external_labels{$key} = "$URL/" . q|node39.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig8/;
+$external_labels{$key} = "$URL/" . q|node40.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig4/;
+$external_labels{$key} = "$URL/" . q|node39.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_kernighan88:_c/;
+$external_labels{$key} = "$URL/" . q|node48.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig6/;
+$external_labels{$key} = "$URL/" . q|node40.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/CLASSDESC_ACCESS/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_02:_insid_mac_os_x/;
+$external_labels{$key} = "$URL/" . q|node48.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/symbolic_enums/;
+$external_labels{$key} = "$URL/" . q|node23.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec2.6/;
+$external_labels{$key} = "$URL/" . q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig11/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/polymorph-example/;
 $external_labels{$key} = "$URL/" . q|node18.html|; 
 $noresave{$key} = "$nosave";
 
@@ -157,40 +157,112 @@ $noresave{$key} = "$nosave";
 # labels from external_latex_labels array.
 
 
-$key = q/fig12/;
-$external_latex_labels{$key} = q|13|; 
+$key = q/CLASSDESC_ACCESS/;
+$external_latex_labels{$key} = q|1.6|; 
 $noresave{$key} = "$nosave";
 
-$key = q//;
-$external_latex_labels{$key} = q|\fi|; 
+$key = q/fig6/;
+$external_latex_labels{$key} = q|7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/slave-method/;
+$external_latex_labels{$key} = q|8.3.1|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig5/;
 $external_latex_labels{$key} = q|6|; 
 $noresave{$key} = "$nosave";
 
+$key = q/fig8/;
+$external_latex_labels{$key} = q|9|; 
+$noresave{$key} = "$nosave";
+
 $key = q/XDR/;
 $external_latex_labels{$key} = q|2|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig3/;
-$external_latex_labels{$key} = q|4|; 
+$key = q/fig4/;
+$external_latex_labels{$key} = q|5|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig10/;
-$external_latex_labels{$key} = q|11|; 
+$key = q/polymorph-example/;
+$external_latex_labels{$key} = q|1|; 
 $noresave{$key} = "$nosave";
 
-$key = q/library-method/;
-$external_latex_labels{$key} = q|1.2|; 
+$key = q/polymorphism/;
+$external_latex_labels{$key} = q|2.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig2/;
+$external_latex_labels{$key} = q|3|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec2.6/;
 $external_latex_labels{$key} = q|10.2.6|; 
 $noresave{$key} = "$nosave";
 
+$key = q/symbolic_enums/;
+$external_latex_labels{$key} = q|5|; 
+$noresave{$key} = "$nosave";
+
 $key = q/fig7/;
 $external_latex_labels{$key} = q|8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig1/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/is_sequence/;
+$external_latex_labels{$key} = q|1.8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig11/;
+$external_latex_labels{$key} = q|12|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ref/;
+$external_latex_labels{$key} = q|2.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig13/;
+$external_latex_labels{$key} = q|14|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig10/;
+$external_latex_labels{$key} = q|11|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig3/;
+$external_latex_labels{$key} = q|4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/library-method/;
+$external_latex_labels{$key} = q|1.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_/;
+$external_latex_labels{$key} = q|<|; 
+$noresave{$key} = "$nosave";
+
+$key = q/classdesc/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/pointers/;
+$external_latex_labels{$key} = q|2.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig14/;
+$external_latex_labels{$key} = q|15|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig15/;
+$external_latex_labels{$key} = q|16|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig9/;
+$external_latex_labels{$key} = q|10|; 
 $noresave{$key} = "$nosave";
 
 $key = q/is_associative_container/;
@@ -201,84 +273,8 @@ $key = q/classdesc_epilogue/;
 $external_latex_labels{$key} = q|1.1|; 
 $noresave{$key} = "$nosave";
 
-$key = q/is_sequence/;
-$external_latex_labels{$key} = q|1.8|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig6/;
-$external_latex_labels{$key} = q|7|; 
-$noresave{$key} = "$nosave";
-
-$key = q/classdesc/;
-$external_latex_labels{$key} = q|1|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig4/;
-$external_latex_labels{$key} = q|5|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig9/;
-$external_latex_labels{$key} = q|10|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig14/;
-$external_latex_labels{$key} = q|15|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig1/;
-$external_latex_labels{$key} = q|2|; 
-$noresave{$key} = "$nosave";
-
-$key = q/slave-method/;
-$external_latex_labels{$key} = q|8.3.1|; 
-$noresave{$key} = "$nosave";
-
-$key = q/symbolic_enums/;
-$external_latex_labels{$key} = q|5|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig8/;
-$external_latex_labels{$key} = q|9|; 
-$noresave{$key} = "$nosave";
-
-$key = q/CLASSDESC_ACCESS/;
-$external_latex_labels{$key} = q|1.6|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ref/;
-$external_latex_labels{$key} = q|2.3|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig15/;
-$external_latex_labels{$key} = q|16|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig2/;
-$external_latex_labels{$key} = q|3|; 
-$noresave{$key} = "$nosave";
-
-$key = q/polymorphism/;
-$external_latex_labels{$key} = q|2.7|; 
-$noresave{$key} = "$nosave";
-
-$key = q/_newlabelxx/;
-$external_latex_labels{$key} = q|\ifx|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig13/;
-$external_latex_labels{$key} = q|14|; 
-$noresave{$key} = "$nosave";
-
-$key = q/pointers/;
-$external_latex_labels{$key} = q|2.1|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig11/;
-$external_latex_labels{$key} = q|12|; 
-$noresave{$key} = "$nosave";
-
-$key = q/polymorph-example/;
-$external_latex_labels{$key} = q|1|; 
+$key = q/fig12/;
+$external_latex_labels{$key} = q|13|; 
 $noresave{$key} = "$nosave";
 
 1;
