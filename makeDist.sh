b3d5a172f65abcf5e17058d26cf7a1fa548e4da5
Russell Standish <hpcoder@hpcoders.com.au>
2023-02-01 15:53:03 +1100

    Update makeDist with json5

--- makeDist.sh
+++ makeDist.sh
@@ -2,7 +2,7 @@
 VERSION=`git describe`
 git archive --format=tar --prefix=classdesc-$VERSION/ HEAD -o /tmp/classdesc-$VERSION.tar
 # add submodules
-cd json_spirit; git archive --format=tar --prefix=classdesc-$VERSION/json_spirit/ HEAD -o /tmp/$$.tar
+cd json5_parser; git archive --format=tar --prefix=classdesc-$VERSION/json_spirit/ HEAD -o /tmp/$$.tar
 tar Af /tmp/classdesc-$VERSION.tar /tmp/$$.tar
 rm /tmp/$$.tar
 gzip -f /tmp/classdesc-$VERSION.tar
