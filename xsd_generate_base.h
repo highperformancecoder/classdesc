ecac2c489390169a3e5fcdf7c80ecf1a55d066f0
Russell Standish <hpcoder@hpcoders.com.au>
2026-02-06 17:33:52 +1100

    Fix non-deterministic XSD parser error by adding namespace="##other ##local" to xs:any wildcard
    This resolves Unique Particle Attribution violations on modern systems like OpenSUSE 16 by ensuring the wildcard does not overlap with elements in the target namespace, satisfying XSD 1.0 determinism requirements.

---
+++ xsd_generate_base.h
@@ -0,0 +1,4 @@
+          xsdDefs[typeBeingaddedTo.back().name]+=
+            "      <xs:any namespace=\"##other ##local\" minOccurs=\"0\" "
+            "maxOccurs=\"unbounded\" processContents=\"lax\"/>
+";
